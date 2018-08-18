6287bceca09200097662fd9399648ff9a02ea716
Danilo Fernandes Nascimento Moreira <danilo.moreira@santander.com.br>
2018-08-18 14:55:20 -0300

    teste conexão banco

--- src/main/java/br/com/passeio_pago/PasseioPagoServiceApplication.java
+++ src/main/java/br/com/passeio_pago/PasseioPagoServiceApplication.java
@@ -1,7 +1,12 @@
 package br.com.passeio_pago;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PasseioPagoServiceApplication {
@@ -9,4 +14,20 @@ public class PasseioPagoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PasseioPagoServiceApplication.class, args);
 	}
+	
+//	@Bean
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
 }
