a9ae0324ab33567922e489dd1d3b8fea6eeedfbf
Danilo Fernandes Nascimento Moreira <danilo.moreira@santander.com.br>
2018-08-18 20:31:04 -0300

    teste de banco e variáveis

--- src/main/java/br/com/passeio_pago/PasseioPagoServiceApplication.java
+++ src/main/java/br/com/passeio_pago/PasseioPagoServiceApplication.java
@@ -1,33 +1,35 @@
 package br.com.passeio_pago;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class PasseioPagoServiceApplication {
 
+	private Logger logger = LoggerFactory.getLogger(PasseioPagoServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PasseioPagoServiceApplication.class, args);
 	}
-	
-//	@Bean
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+
+	@Bean
+	@Profile(value = { "devl", "prod" })
+	public BasicDataSource dataSource() throws URISyntaxException {
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+		basicDataSource.setUsername(System.getenv("SPRING_DATASOURCE_USERNAME"));
+		basicDataSource.setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
+		logger.debug("\n\ndebug");
+		logger.info("\n\ninfo");
+		logger.error("\n\nerror");
+		logger.warn("\n\nwarn");
+		return basicDataSource;
+	}
 }
