73830bd32dda347d95fd260c9a1310fe8e0ad0b2
Danilo Fernandes Nascimento Moreira <danilo.moreira@santander.com.br>
2018-08-26 17:58:23 -0300

    testes

--- src/main/java/br/com/passeio_pago/TesteController.java
+++ src/main/java/br/com/passeio_pago/TesteController.java
@@ -1,6 +1,10 @@
 package br.com.passeio_pago;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +22,14 @@ public class TesteController {
 		repository.save(team);
 
 		return "ok";
+	}
+	
+	@GetMapping(path = {"/buscar"})
+	public ResponseEntity<List<Team>> buscar() {
+
+		List<Team> findAll = repository.findAll();
+
+		return new ResponseEntity<List<Team>>(findAll, HttpStatus.OK);
 	}
 
 }
