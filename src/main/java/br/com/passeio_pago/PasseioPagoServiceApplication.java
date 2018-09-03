2833fd6ac52830f2ea4d0b9595b0bc1932f1013c
Danilo Fernandes Nascimento Moreira <danilo.moreira@santander.com.br>
2018-09-03 00:02:59 -0300

    criado o demo de upload de arquivos

--- src/main/java/br/com/passeio_pago/PasseioPagoServiceApplication.java
+++ src/main/java/br/com/passeio_pago/PasseioPagoServiceApplication.java
@@ -7,16 +7,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import br.com.passeio_pago.file_handler.property.FileStorageProperties;
 
 /**
  * @author danilo-moreira
  *
  */
 @SpringBootApplication
+@EnableConfigurationProperties(value= {FileStorageProperties.class})
 public class PasseioPagoServiceApplication {
 
 	private Logger logger = LoggerFactory.getLogger(PasseioPagoServiceApplication.class);
