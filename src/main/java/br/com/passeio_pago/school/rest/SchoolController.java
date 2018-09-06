371b56c09026ae6c26a8ce8a77559af7d7281b7c
Danilo Fernandes Nascimento Moreira <danilo.moreira@santander.com.br>
2018-09-06 12:02:58 -0300

    buscas de escolas por id

--- src/main/java/br/com/passeio_pago/school/rest/SchoolController.java
+++ src/main/java/br/com/passeio_pago/school/rest/SchoolController.java
@@ -3,6 +3,7 @@ package br.com.passeio_pago.school.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +22,22 @@ public class SchoolController {
 	@Autowired
 	private SchoolService service;
 
-	@ApiOperation(value = "Search list of schools. Return the top 50 that most approach the criterias. Note that no parameter is required.", tags = "schools")
+	@ApiOperation(value = "Search of schools. Return the top 50 that most approach the criterias. Note that no parameter is required.", tags = "schools")
 	@GetMapping("/")
-	public ResponseEntity<School[]> getSchools(
-			@ApiParam(required=false, example="maria theodora pedreira de freitas", value="The name of the school.")
-			@RequestParam(required = false, name = "nome") String nome,
-			@ApiParam(required=false, example="Pública", value="Type of school.", allowableValues="Pública,Privada")
-			@RequestParam(required = false, name = "rede") String rede,
-			@ApiParam(required=false, example="Barueri", value="The city of the school.")
-			@RequestParam(required = false, name = "municipio") String municipio,
-			@ApiParam(required=false, example="SP", value="The state of the school.", allowableValues="AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO")
-			@RequestParam(required = false, name = "uf") String uf,
-			@ApiParam(required=false, example="Municipal", allowableValues="Estadual,Municipal,Federal,Privada")
-			@RequestParam(required = false, name = "esferaAdministrativa") String esferaAdministrativa) {
-		School[] schools = service.getSchools(nome, rede, municipio, uf, esferaAdministrativa);
+	public ResponseEntity<School[]> getSchools(@ApiParam(required = false, example = "maria theodora", value = "The name of the school.") @RequestParam(required = false, name = "nome") String nome,
+			@ApiParam(required = false, example = "Pública", value = "Type of school.", allowableValues = "Pública,Privada") @RequestParam(required = false, name = "rede") String rede,
+			@ApiParam(required = false, example = "Barueri", value = "The city of the school.") @RequestParam(required = false, name = "municipio") String municipio,
+			@ApiParam(required = false, example = "SP", value = "The state of the school.", allowableValues = "AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO") @RequestParam(required = false, name = "uf") String uf,
+			@ApiParam(required = false, example = "Municipal", allowableValues = "Estadual,Municipal,Federal,Privada") @RequestParam(required = false, name = "esferaAdministrativa") String esferaAdministrativa) {
+		School[] schools = service.getSchoolsByAddress(nome, rede, municipio, uf, esferaAdministrativa);
 		return ResponseEntity.ok(schools);
 	}
+
+	@ApiOperation(value = "Search a schools by id.", tags = "schools")
+	@GetMapping("/{schoolId}")
+	public ResponseEntity<School> getSchools(@PathVariable(name = "schoolId") Long schoolId) {
+		School schools = service.getSchoolsById(schoolId);
+		return ResponseEntity.ok(schools);
+	}
+
 }
--- src/main/java/br/com/passeio_pago/school/service/SchoolService.java
+++ src/main/java/br/com/passeio_pago/school/service/SchoolService.java
@@ -27,17 +27,20 @@ public class SchoolService {
 	@Autowired
 	private String tcuSchoolRestWebService;
 
-	public void getSchoolsById(String id) {
-
-	}
-
-	public void getSchoolsInTheArea(String id) {
-
-	}
-
-	public School[] getSchools(String nome, String rede, String municipio, String uf, String esferaAdministrativa) {
+	public School getSchoolsById(Long schoolId) {
 		try {
-			URI uri = new URI(buildUri(nome, rede, municipio, uf, esferaAdministrativa));
+			URI uri = new URI(tcuSchoolRestWebService + schoolId + "?" + buildUri(null, null, null, null, null, 0));
+			School school = restTemplate.getForObject(uri, School.class);
+			school.getLinks()[0].setHref(ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString() + schoolId);
+			return school;
+		} catch (Exception e) {
+			throw new ElementNotFoundException();
+		}
+	}
+
+	public School[] getSchoolsByAddress(String nome, String rede, String municipio, String uf, String esferaAdministrativa) {
+		try {
+			URI uri = new URI(tcuSchoolRestWebService + "?" + buildUri(nome, rede, municipio, uf, esferaAdministrativa, 50));
 			School[] listOfSchools = restTemplate.getForObject(uri, School[].class);
 			Stream.of(listOfSchools).forEach(school -> {
 				String href = school.getLinks()[0].getHref();
@@ -50,16 +53,16 @@ public class SchoolService {
 		}
 	}
 
-	public String buildUri(String nome, String rede, String municipio, String uf, String esferaAdministrativa) throws UnsupportedEncodingException {
-		Map<String, Object> schoolUriVariables = getSchoolUriVariablesToSearch(nome, rede, municipio, uf, esferaAdministrativa);
-		String stringQuery = schoolUriVariables.toString();
-		stringQuery = StringUtils.replace(stringQuery, "{", "");
-		stringQuery = StringUtils.replace(stringQuery, "}", "");
-		stringQuery = StringUtils.replace(stringQuery, ", ", "&");
-		return tcuSchoolRestWebService + "?" + stringQuery;
+	private String buildUri(String nome, String rede, String municipio, String uf, String esferaAdministrativa, int quantidadeDeItens) throws UnsupportedEncodingException {
+		Map<String, Object> schoolUriVariables = getSchoolUriVariablesToSearch(nome, rede, municipio, uf, esferaAdministrativa, quantidadeDeItens);
+		String queryString = schoolUriVariables.toString();
+		queryString = StringUtils.replace(queryString, "{", "");
+		queryString = StringUtils.replace(queryString, "}", "");
+		queryString = StringUtils.replace(queryString, ", ", "&");
+		return queryString;
 	}
 
-	public Map<String, Object> getSchoolUriVariablesToSearch(String nome, String rede, String municipio, String uf, String esferaAdministrativa) throws UnsupportedEncodingException {
+	private Map<String, Object> getSchoolUriVariablesToSearch(String nome, String rede, String municipio, String uf, String esferaAdministrativa, int quantidadeDeItens) throws UnsupportedEncodingException {
 		Map<String, Object> uriVariables = new HashMap<String, Object>();
 		if (!StringUtils.isBlank(nome)) {
 			uriVariables.put("nome", URLEncoder.encode(nome.toUpperCase(), StandardCharsets.UTF_8.toString()));
@@ -78,7 +81,9 @@ public class SchoolService {
 			uriVariables.put("esferaAdministrativa", URLEncoder.encode(esferaAdministrativa, StandardCharsets.UTF_8.toString()));
 		}
 		uriVariables.put("campos", getSchoolFildsToBeReturned());
-		uriVariables.put("quantidadeDeItens", 50);
+		if (quantidadeDeItens != 0) {
+			uriVariables.put("quantidadeDeItens", quantidadeDeItens);
+		}
 		return uriVariables;
 	}
 
